b970c70fd00af6d12edd958453b2d5adde4e2931
Daniel Shusta and Sam Serrano <pair+dshusta+sam@pivotal.io>
2014-12-30 17:29:36 -0800

    Rename BlogScoreKeeper to ScoreKeeper

--- ScoreKeeper/AppDelegate.h
+++ ScoreKeeper/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  BlogScoreKeeper
-//
-//  Created by pivotal on 4/2/14.
-//  Copyright (c) 2014 PivotalBeach. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- ScoreKeeper/main.m
+++ ScoreKeeper/main.m
@@ -1,13 +1,4 @@
-//
-//  main.m
-//  BlogScoreKeeper
-//
-//  Created by pivotal on 4/2/14.
-//  Copyright (c) 2014 PivotalBeach. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[])
